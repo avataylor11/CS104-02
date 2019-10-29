b7b53154510022a5e2acf43cad738442703e86d3
Ava Taylor <s1270111@monmouth.edu>
2019-10-29 14:23:45 -0400

    Create PythonRichterScale.py

---
+++ PythonRichterScale.py
@@ -0,0 +1,44 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 8.1
+Most Structures Fall
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 8.0
+Most Structures Fall
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 7.1
+Many Buildings Destroyed
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 7.0
+Many Buildings Destroyed
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 6.1
+Many Buildings Considerably Damaged, Some Collapse
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 6.0
+Many Buildings Considerably Damaged, Some Collapse
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 4.6
+Damage To Poorly Constructed Buildings
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 4.5
+Damage To Poorly Constructed Buildings
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: 4.4
+No Destruction Of Buildings
+>>> 
+============== RESTART: /Users/avataylor/Downloads/pyRichter.py ==============
+Enter the Richter scale value: -4.6
+No Destruction Of Buildings
+>>> 
